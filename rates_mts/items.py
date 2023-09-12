28e1a9d98e63e54293972868d09a1de2c1ebae99
Илья Ватютов <vatut007@ya.ru>
2023-09-12 11:28:52 +0300

    Change structure folders

--- rates_mts/items.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-class RatesMtsItem(scrapy.Item):
-    name = scrapy.Field()
-    description = scrapy.Field()
-    price = scrapy.Field()
-    quota = scrapy.Field()
-    options = scrapy.Field()
