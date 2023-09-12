1cbe88fe7e53f4e9ef8e655cadbc8fa890aa8153
Илья Ватютов <vatut007@ya.ru>
2023-09-12 11:24:49 +0300

    Change structure folders

---
+++ parser_web/rates_mts/items.py
@@ -0,0 +1,8 @@
+import scrapy
+
+class RatesMtsItem(scrapy.Item):
+    name = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()
+    quota = scrapy.Field()
+    options = scrapy.Field()
